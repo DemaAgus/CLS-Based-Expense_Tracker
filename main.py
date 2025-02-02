1ea58f26c5851a1d95e03ee9b527e605f80456dd
Agustin De Marchi <agusdemarchi03@gmail.com>
2025-02-02 16:26:37 -0300

    Feat: make Menu() function

---
+++ main.py
@@ -0,0 +1,20 @@
+def menu():
+    menu = ['Add.','Edit.','Delete.','Total of the month.','Export data.']
+    print("Select one option:")
+    for i in range(len(menu)):
+        print(f'{i+1}. {menu[i]}')
+    option = input("Option:")
+    match option:
+        case '1':
+            add()
+        case '2':
+            edit()
+        case '3':
+            delete()
+        case '4':
+            total()
+        case '5':
+            export()
+        case _:
+            print("Invalid option")
+            menu()
